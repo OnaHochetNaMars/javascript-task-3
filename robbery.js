99678f0b4e950f54956e6f7da35f9e06084977b0
Лера <onahochetnamars@yandex.ru>
2016-10-26 23:32:08 +0500

    Update robbery.js

--- robbery.js
+++ robbery.js
@@ -4,7 +4,63 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-exports.isStar = true;
+exports.isStar = false;
+
+var DaysOfWeek = {
+    'ПН': 1,
+    'ВТ': 2,
+    'СР': 3,
+    'ЧТ': 4,
+    'ПТ': 5,
+    'СБ': 6,
+    'ВС': 7
+}
+
+function toDate (date) {
+    var dayOfWeek = date.substring(0, 2);
+    var day = DaysOfWeek[dayOfWeek];
+    var hours = parseInt (date.substring(3, 5)) - parseInt (date.substring(8));
+    var minutes = parseInt (date.substring(6, 8));
+    return new Date (2016, 09, day, hours, minutes);
+}
+
+function newSchedule (schedule) {
+    var schedule1 = {};
+    for (var key in schedule) {
+        var n = (schedule[key]).length;
+        schedule1[key] = [];
+        for (var i = 0; i < n; i++) {
+            schedule1[key][i] = {
+                from: toDate (schedule[key][i].from),
+                to: toDate (schedule[key][i].to)
+            }
+            schedule1[key].len = n;
+        }
+    }
+    return schedule1;
+}
+
+function maxFrom (date1, date2, date3) {
+    if (date1.from > date2.from && date1.from > date3.from) return date1.from;
+    if (date2.from > date3.from) return date2.from;
+    return date3.from;
+}
+
+function minTo (date1, date2, date3) {
+    if (date1.to < date2.to && date1.to < date3.to) return date1.to;
+    if (date2.to < date3.to) return date2.to;
+    return date3.to;
+}
+
+function maxDate (date1, date2) {
+    if (date1 > date2) { return date1;}
+    return date2;
+}
+
+function minDate (date1, date2) {
+    if (date1 < date2) { return date1;}
+    return date2;
+}
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -14,8 +70,13 @@ exports.isStar = true;
  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
  * @returns {Object}
  */
+
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    //console.info(schedule, duration, workingHours);
+    //переводим расписание в формат Date()
+    var schedule1 = {};
+    schedule1 = newSchedule (schedule);
+    console.info(schedule1);
 
     return {
 
@@ -36,15 +97,6 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          */
         format: function (template) {
             return template;
-        },
-
-        /**
-         * Попробовать найти часы для ограбления позже [*]
-         * @star
-         * @returns {Boolean}
-         */
-        tryLater: function () {
-            return false;
         }
     };
 };
