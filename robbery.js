d4f8f5459ffd32891bd62b11325d5459f622ea12
Лера <onahochetnamars@yandex.ru>
2016-10-28 21:39:49 +0500

    Update robbery.js

--- robbery.js
+++ robbery.js
@@ -114,23 +114,23 @@ function gangFreeTime(schedule) {
     var D = schedule.Danny.len;
     var R = schedule.Rusty.len;
     var L = schedule.Linus.len;
-    function func() {
+    function func(i, j, k) {
+        var a = maxFrom (schedule.Danny[i], schedule.Rusty[j], schedule.Linus[k]);
+        var b = minTo (schedule.Danny[i], schedule.Rusty[j], schedule.Linus[k]);
+        if (b > a) {
+            freeTime.push({
+                from: a,
+                to: b
+            });
+        }
+    }
     for (var i = 0; i < D; i++) {
         for (var j = 0; j < R; j++) {
             for (var k = 0; k < L; k++) {
-                var a = maxFrom (schedule.Danny[i], schedule.Rusty[j], schedule.Linus[k]);
-                var b = minTo (schedule.Danny[i], schedule.Rusty[j], schedule.Linus[k]);
-                if (b > a) {
-                    freeTime.push({
-                        from: a,
-                        to: b
-                    });
-                }
+                func(i, j, k);
             }
         }
     }
-    }
-    func();
 
     return freeTime;
 }
@@ -144,7 +144,7 @@ function findTimeForRobbery(freeTime, workingHours) {
     var bank = [];
     hours[0] = parseInt (date[0].substring (0, 2)) - parseInt (date[0].substring (5));
     hours[1] = parseInt (date[1].substring (0, 2)) - parseInt (date[1].substring (5));
-    minutes[0] = parseInt (date[0].substring (3, 5)); 
+    minutes[0] = parseInt (date[0].substring (3, 5));
     minutes[1] = parseInt (date[1].substring (3, 5));
     for (var i = 0; i < n; i++) {
         var day = freeTime[i].from.getDate();
@@ -159,7 +159,7 @@ function findTimeForRobbery(freeTime, workingHours) {
             res.push({
                 from: a,
                 to: b
-            })
+            });
         }
     }
 
@@ -195,11 +195,12 @@ function formateDate(date, gmt) {
     if (hours < 10) {
         hours = '0' + hours;
     }
-    hours = '' + hours;
+    hours = String(hours);
     if (minutes < 10) {
         minutes = '0' + minutes;
     }
-    minutes = '' + minutes;
+    minutes = String(minutes);
+
     return [day, hours, minutes];
 }
 
@@ -219,7 +220,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     var freeTime = gangFreeTime (freeSchedule);
     var timeForRobbery = findTimeForRobbery (freeTime, workingHours);
     var timeToRobbery = mainFunction (timeForRobbery, duration);
-    var gmt = parseInt (workingHours.from.substring(6));
+    var gmt = parseInt (workingHours.from.substring(5));
 
     return {
 
@@ -228,7 +229,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            // console.log (this.time);
+
             return (timeToRobbery !== null);
         },
 
